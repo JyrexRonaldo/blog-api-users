1f852b70c057e5ba9cee9f3dd31d8a99b9354374
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-04-12 10:17:33 +0000

    Add login form

---
+++ src/Components/LogInForm/LoginForm.jsx
@@ -0,0 +1,22 @@
+function LoginForm() {
+  return (
+    <>
+      <div>
+        <h1>Login</h1>
+        <form action="">
+            <p>Username</p>
+            <br />
+            <input type="text" name="" id="" />
+            <p>Password</p>
+            <br />
+            <input type="password" name="" id="" />
+            <button>Login</button>
+        </form>
+        <p>Don't have an account? </p>
+      </div>
+    </>
+  );
+}
+
+
+export default LoginForm
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,20 @@
 import App from "./Components/App/App";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
+import LoginForm from "./Components/LogInForm/LogInForm";
 
 
 const routes = [
-    {
-        path: "/",
-        element: <App />
-    }
-]
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/login",
+        element: <LoginForm />,
+      },
+    ],
+  },
+];
 
-export default routes
+export default routes;
