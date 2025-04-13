63d926846cb8a33caca5b96cd62742225b3aff2a
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-04-13 21:11:14 +0000

    Add styling to register component

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -32,36 +32,48 @@ function RegisterForm() {
 
     return (
         <>
-            <div>
-                <h1 >Register</h1>
-                <form>
-                    <div>
-                        <label htmlFor="username">Username</label>
-                        <br />
+            <div className="mt-30 flex w-80 flex-col items-center self-center rounded-2xl bg-neutral-700 p-8">
+                <h1 className="text-2xl font-extrabold">Register an account</h1>
+                <form className="flex flex-col gap-2">
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="username" className="font-light">
+                            Username
+                        </label>
                         <input
                             type="text"
                             name="username"
                             id="username"
                             value={username}
                             onChange={handleUsernameInput}
+                            className="w-full bg-neutral-800"
                         />
                     </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <br />
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="password" className="font-light">
+                            Password
+                        </label>
+
                         <input
                             type="password"
                             name="password"
                             id="password"
                             value={password}
                             onChange={handlePasswordInput}
+                            className="w-full bg-neutral-800"
                         />
                     </div>
-                    <button type="button" onClick={handleRegisterButton}>
+                    <button
+                        type="button"
+                        onClick={handleRegisterButton}
+                        className="w-full bg-blue-800 p-1.5"
+                    >
                         Register
                     </button>
                     <p>
-                        Already have an account? <Link to="/login">Login</Link>
+                        Already have an account?{' '}
+                        <Link to="/login" className="text-blue-500">
+                            Login
+                        </Link>
                     </p>
                 </form>
             </div>
