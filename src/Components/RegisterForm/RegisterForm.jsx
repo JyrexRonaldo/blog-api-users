b9a50550e7c19dd29ce3ec572b23d28039187dcc
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-04-13 07:58:00 +0000

    Add user registration functionality

--- src/Components/RegisterForm/RegisterForm.jsx
+++ src/Components/RegisterForm/RegisterForm.jsx
@@ -1,22 +1,66 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function RegisterForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameInput = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordInput = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleRegisterButton = async () => {
+    
+    try {
+      const response = await fetch("http://localhost:3000/auth/sign-up", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({username,password}),
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div>
         <h1>Register</h1>
-        <form action="">
+        <form>
           <div>
             <label htmlFor="username">Username</label>
             <br />
-            <input type="text" name="username" id="username" />
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={handleUsernameInput}
+            />
           </div>
           <div>
             <label htmlFor="password">Password</label>
             <br />
-            <input type="password" name="password" id="password" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordInput}
+            />
           </div>
-          <button>Register</button>
+          <button type="button" onClick={handleRegisterButton}>
+            Register
+          </button>
           <p>
             Already have an account? <Link to="/login">Login</Link>
           </p>
