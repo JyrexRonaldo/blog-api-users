20da0c954e1c2e234cd671117dd65548038d6c11
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-04-13 11:53:20 +0000

    Add user log in functionality

--- src/Components/LogInForm/LoginForm.jsx
+++ src/Components/LogInForm/LoginForm.jsx
@@ -1,22 +1,60 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+
+
 
 function LoginForm() {
+
+  const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate()
+  
+    const handleUsernameInput = (e) => {
+      setUsername(e.target.value);
+    };
+  
+    const handlePasswordInput = (e) => {
+      setPassword(e.target.value);
+    };
+
+    const handleLoginButton = async () => {
+    
+      try {
+        const response = await fetch("http://localhost:3000/auth/log-in", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({username,password}),
+        });
+  
+        const data = await response.json();
+        console.log(data);
+        localStorage.setItem("userToken", `${data.token}`)
+        console.log(`Local storage variable : ${localStorage.getItem("userToken")}`)
+        navigate("/")
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
   return (
     <>
       <div>
         <h1>Login</h1>
-        <form action="">
+        <form>
           <div>
             <label htmlFor="username">Username</label>
             <br />
-            <input type="text" name="username" id="username" />
+            <input type="text" name="username" id="username" value={username} onChange={handleUsernameInput} />
           </div>
           <div>
             <label htmlFor="password">Password</label>
             <br />
-            <input type="password" name="password" id="password" />
+            <input type="password" name="password" id="password" value={password} onChange={handlePasswordInput} />
           </div>
-          <button>Login</button>
+          <button type="button" onClick={handleLoginButton}>Login</button>
         </form>
         <p>
           Don't have an account? <Link to="/register">Register</Link>
