8cdbdd9f8d6399b49a3efe0d8ff748212b59d57e
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-04-13 22:09:27 +0000

    Add styling to PostCard component

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,13 +1,21 @@
-function PostCard(authorName, dateCreated, postTitle, commentsNumber) {
+import { Link } from 'react-router-dom'
+
+function PostCard({ authorName, dateCreated, postTitle, commentsNumber }) {
     return (
-        <>
-            <p>{authorName}</p>
-            <p>{dateCreated}</p>
-            <p>{postTitle}</p>
-            <p>
-                {commentsNumber} {commentsNumber > 1 ? 'Comments' : 'Comment'}
-            </p>
-        </>
+        <div className="flex flex-col gap-3 rounded-[12px] bg-neutral-800 p-5.5">
+            <div className="flex gap-3.5">
+                <p className="text-[0.8rem] font-extrabold">{authorName}</p>
+                <p className="text-[0.8rem] font-extralight">{dateCreated}</p>
+            </div>
+            <p className="text-4xl font-bold">{postTitle}</p>
+            <div className="flex gap-3.5 font-extralight">
+                <p>
+                    {commentsNumber}{' '}
+                    {commentsNumber > 1 ? 'Comments' : 'Comment'}
+                </p>
+                <Link to="#">Read</Link>
+            </div>
+        </div>
     )
 }
 
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -2,8 +2,14 @@ import PostCard from '../PostCard/PostCard'
 
 function Posts() {
     return (
-        <div className="mt-13 w-180 self-center">
+        <div className="mt-13 flex w-180 flex-col self-center gap-5">
             <h1 className="text-4xl font-extrabold">Posts</h1>
+            <PostCard
+                authorName={'jyrex'}
+                dateCreated={'March 22, 2025'}
+                postTitle={'Test post'}
+                commentsNumber={34}
+            />
             <PostCard
                 authorName={'jyrex'}
                 dateCreated={'March 22, 2025'}
