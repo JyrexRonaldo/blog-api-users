03c7241fd8f3563f5889a38cb09f5b05bead18ae
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-04-13 21:08:55 +0000

    Add styling to app component

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -3,12 +3,16 @@ import { Link, Outlet } from 'react-router-dom'
 function App() {
     return (
         <>
-            <div  >
-                <nav>
+            <div className="flex h-screen flex-col bg-neutral-900 text-white">
+                <nav className="flex h-20 items-center justify-around">
                     <Link to="/">
-                        <p>OdinBlog</p>
+                        <p className="text-3xl font-extrabold text-blue-500">
+                            OdinBlog
+                        </p>
                     </Link>
-                    <button>Log out</button>
+                    <button className="cursor-pointer rounded-[10px] border border-neutral-600 p-2">
+                        Log out
+                    </button>
                 </nav>
                 <Outlet />
             </div>
