9c7a2d0c7b53d7c2e9cc41501529a39add21b4c5
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-04-17 17:37:47 +0000

    Add post render logic

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,30 +1,49 @@
 import PostCard from '../PostCard/PostCard'
 import PostItem from '../PostItem/PostItem'
+import PostDataContext from '../PostDataContext/PostDataContext'
+import { useContext } from 'react'
 
 function Posts() {
-    return (
-        <div className="mt-13 flex w-180 flex-col gap-5 self-center">
-            {/* <h1 className="text-4xl font-extrabold">Posts</h1> */}
-            {/* <PostCard
-                authorName={'jyrex'}
-                dateCreated={'March 22, 2025'}
-                postTitle={'Test post'}
-                commentsNumber={34}
-            /> */}
-            {/* <PostCard
-                authorName={'jyrex'}
-                dateCreated={'March 22, 2025'}
-                postTitle={'Test post'}
-                commentsNumber={34}
-            /> */}
+    const { postsData, error, loading } = useContext(PostDataContext)
+    if (loading)
+        return (
+            <div>
+                <p>Loading...</p>
+            </div>
+        )
+    if (error)
+        return (
+            <div>
+                <p>A network error was encountered</p>
+            </div>
+        )
 
-            <PostItem
+        console.log(postsData)
+
+        const postCards = postsData.map((post) => {
+            return (
+                <PostCard key={post.id}
+                authorName={post.author.username}
+                dateCreated={post.createdAt}
+                postTitle={post.title}
+                commentsNumber={post._count.comments}
+            />
+            )
+        })
+
+    return (
+        <div className="mt-2 flex w-180 flex-col gap-5 self-center">
+            <h1 className="text-4xl font-extrabold">Posts</h1>
+
+            {postCards}
+
+            {/* <PostItem
                 authorName={'admin'}
                 dateCreated="March 22, 2025"
                 postTitle="Test post"
                 commentsNumber={7}
                 postBody="Lorem Ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            />
+            /> */}
         </div>
     )
 }
