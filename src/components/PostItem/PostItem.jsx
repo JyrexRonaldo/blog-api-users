25edb43e8cbb429b5d99cb6be03bfc584b481e3c
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-04-17 10:44:39 +0000

    Add icons to comment button in PostItem component

--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Comment from '../Comment/Comment'
+import commentIcon from '/commentIcon.svg'
 
 function PostItem({
     authorName,
@@ -16,31 +17,48 @@ function PostItem({
 
     return (
         <div className="flex flex-col gap-3 rounded-[12px] bg-neutral-800 p-5.5">
-            <p className='text-4xl font-extrabold'>{postTitle}</p>
-            <div className='flex gap-4'>
+            <p className="text-4xl font-extrabold">{postTitle}</p>
+            <div className="flex gap-4">
                 <p>{authorName}</p>
                 <p>Posted on: {dateCreated}</p>
             </div>
             <p>{postBody}</p>
-            <p onClick={handleCommentDisplay}>
-                {commentsNumber} {commentsNumber > 1 ? 'Comments' : 'Comment'}
-            </p>
+            <button
+                onClick={handleCommentDisplay}
+                className="flex items-center gap-1.5"
+            >
+                <img
+                    className="h-auto w-3.5"
+                    src={commentIcon}
+                    alt="comment icon"
+                />
+                <p>
+                    {commentsNumber}{' '}
+                    {commentsNumber > 1 ? 'Comments' : 'Comment'}
+                </p>
+            </button>
 
             {show && (
                 <div className="flex flex-col gap-3">
                     <p>Comments ({commentsNumber})</p>
                     <div className="flex items-center gap-2">
                         <textarea
-                            className="resize-none bg-neutral-700 py-1 px-2 rounded-[7px]"
+                            className="resize-none rounded-[7px] bg-neutral-700 px-2 py-1"
                             name=""
                             id=""
                             placeholder="Leave a comment..."
                             cols="70"
                             rows="2"
                         ></textarea>
-                        <button className="bg-blue-500 p-2 rounded-[7px]">Send</button>
+                        <button className="rounded-[7px] bg-blue-500 p-2">
+                            Send
+                        </button>
                     </div>
-                    <Comment author="admin" createdAt="March 22, 2025" comment="Test comment." />
+                    <Comment
+                        author="admin"
+                        createdAt="March 22, 2025"
+                        comment="Test comment."
+                    />
                 </div>
             )}
         </div>
